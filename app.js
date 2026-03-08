7a331b27353b0546651597329dfde0adcaac08a9
preetham-ui <potlapreetham2007@gmail.com>
2026-03-08 11:15:30 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,47 @@
-function planTrip() {
+function planTrip(){
 
-  const from = document.getElementById("from").value;
-  const destination = document.getElementById("destination").value;
-  const budget = parseInt(document.getElementById("budget").value);
-  const people = parseInt(document.getElementById("people").value);
-  const days = parseInt(document.getElementById("days").value);
-  const result = document.getElementById("result");
+const from = document.getElementById("from").value;
+const destination = document.getElementById("destination").value;
+const budget = parseInt(document.getElementById("budget").value);
+const people = document.getElementById("people").value;
+const days = document.getElementById("days").value;
 
-  if (!from || !destination || !budget || !people || !days) {
-    result.innerHTML = "Please fill all fields.";
-    return;
-  }
+const result = document.getElementById("result");
+
+if(!from || !destination || !budget || !people || !days){
+result.innerHTML = "Please fill all fields";
+return;
+}
+
 const plans = generateTripOptions(budget);
 
-const budgetPlan = plans.budgetTrip;
-const balancedPlan = plans.balancedTrip;
-const comfortPlan = plans.comfortTrip;
+const flightLink = "https://www.skyscanner.co.in/transport/flights/";
+const hotelLink = "https://www.booking.com/searchresults.html?ss=" + destination;
 
-  let flightLink = `https://www.skyscanner.co.in/transport/flights/`;
-  let hotelLink = `https://www.booking.com/searchresults.html?ss=${destination}`;
+result.innerHTML = `
+<div class="resultCard">
 
-  result.innerHTML = `
-<h3>${from} → ${destination}</h3>
+<h2>${from} → ${destination}</h2>
 
-<h4>Budget Trip</h4>
-Travel: ₹${budgetPlan.travel}<br>
-Stay: ₹${budgetPlan.stay}<br>
-Food: ₹${budgetPlan.food}<br>
+<h3>Budget Plan</h3>
+Travel ₹${plans.budgetTrip.travel}<br>
+Stay ₹${plans.budgetTrip.stay}<br>
+Food ₹${plans.budgetTrip.food}<br><br>
 
-<h4>Balanced Trip</h4>
-Travel: ₹${balancedPlan.travel}<br>
-Stay: ₹${balancedPlan.stay}<br>
-Food: ₹${balancedPlan.food}<br>
+<h3>Balanced Plan</h3>
+Travel ₹${plans.balancedTrip.travel}<br>
+Stay ₹${plans.balancedTrip.stay}<br>
+Food ₹${plans.balancedTrip.food}<br><br>
 
-<h4>Comfort Trip</h4>
-Travel: ₹${comfortPlan.travel}<br>
-Stay: ₹${comfortPlan.stay}<br>
-Food: ₹${comfortPlan.food}<br>
+<h3>Comfort Plan</h3>
+Travel ₹${plans.comfortTrip.travel}<br>
+Stay ₹${plans.comfortTrip.stay}<br>
+Food ₹${plans.comfortTrip.food}<br><br>
+
+<a href="${flightLink}" target="_blank">View Flights</a><br>
+<a href="${hotelLink}" target="_blank">View Hotels</a>
+
+</div>
 `;
+
+}
